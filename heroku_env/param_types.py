168cd5ecaba8e92e332b7fd0350bab310d87523b
viseshrp <viseshrprasad@gmail.com>
2018-12-24 14:39:35 -0500

    APIKeyParamType: return value on converting

--- heroku_env/param_types.py
+++ heroku_env/param_types.py
@@ -12,7 +12,10 @@ class APIKeyParamType(click.ParamType):
     name = 'API Key Param Type'
 
     def convert(self, value, param, ctx):
-        is_valid = re.match(r'[0-9a-f]{36}', value)
+        cleaned_value = value.strip()
+        is_valid = re.match(r'[0-9-a-f]{36}', cleaned_value)
 
         if not is_valid:
             self.fail('Your API Key has to be a 36-character hexadecimal string', param, ctx)
+
+        return cleaned_value
