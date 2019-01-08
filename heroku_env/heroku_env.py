e03aa4dbb098786611564424b42c0506d7f8112f
viseshrp <viseshrprasad@gmail.com>
2019-01-08 16:46:36 -0500

    skip blank lines

--- heroku_env/heroku_env.py
+++ heroku_env/heroku_env.py
@@ -58,39 +58,39 @@ def upload_env(app_name, env_file, set_alt):
 
         for line in e:
             line = line.strip()
+            if line:
+                # check comments
+                if line.startswith("#"):
+                    # enable --set-alt
+                    if set_alt and "alt_value=" in line:
+                        alt_value = line.split("alt_value=", 1)[1]
+                        if alt_value:
+                            # use this value for the next env var
+                            use_alt = True
+                    # any kind of comment warrants a skip
+                    continue
 
-            # check comments
-            if line.startswith("#"):
-                # enable --set-alt
-                if set_alt and "alt_value=" in line:
-                    alt_value = line.split("alt_value=", 1)[1]
-                    if alt_value:
-                        # use this value for the next env var
-                        use_alt = True
-                # any kind of comment warrants a skip
-                continue
+                # set env vars
+                if "=" in line:
+                    kv_pair = line.split("=", 1)
+                    if len(kv_pair) > 1:  # has to at least be of form: k=
+                        key = kv_pair[0]
 
-            # set env vars
-            if "=" in line:
-                kv_pair = line.split("=", 1)
-                if len(kv_pair) > 1:  # has to at least be of form: k=
-                    key = kv_pair[0]
+                        if use_alt:
+                            value = alt_value
+                            # reset
+                            use_alt = False
+                        else:
+                            value = kv_pair[1]
 
-                    if use_alt:
-                        value = alt_value
-                        # reset
-                        use_alt = False
-                    else:
-                        value = kv_pair[1]
+                        # an empty value is fine
+                        if key:
+                            config_dict[key] = value
+                            # verify
+                            click.secho(u'\u2713 ' + key, fg='green', bold=True)
 
-                    # an empty value is fine
-                    if key:
-                        config_dict[key] = value
-                        # verify
-                        click.secho(u'\u2713 ' + key, fg='green', bold=True)
-
-            else:
-                click.echo("Skipping line : Not of the form key=value")
+                else:
+                    click.echo("Skipping line : Not of the form key=value")
 
     # update
     update_result = update_config_vars(config_dict, app_instance)
